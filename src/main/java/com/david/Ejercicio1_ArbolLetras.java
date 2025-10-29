cab280dfb8d833f24950b0273b21a6360d209a06
David Gutiérrez <empirio14@alumnos.ilerna.com>
2025-10-29 07:16:57 +0100

    Primer commit - Estructura inicial del proyecto

---
+++ src/main/java/com/david/Ejercicio1_ArbolLetras.java
@@ -0,0 +1,4 @@
+package com.david;
+
+public class Ejercicio1_ArbolLetras {
+}
---
+++ src/main/java/com/david/Main.java
@@ -0,0 +1,7 @@
+package com.david;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
